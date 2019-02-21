7a8b2e0f871d068bdcd4e8e7126e31ccec9ee358
Fábio Morais <fabiouds@hotmail.com>
2019-02-21 22:43:57 +0000

    aula1

---
+++ JAVASCRIPT/aula1.js
@@ -0,0 +1,16 @@
+console.log('string' + 'comp');
+console.log(5);
+console.log('string'.length);
+console.log('string'.toUpperCase());
+console.log(Number.isInteger(5));
+
+var myName= 'Fabio'
+var myCity= 'porto'
+console.log(`My name is ${myName}. My favorite city is ${myCity}`)
+
+
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);
