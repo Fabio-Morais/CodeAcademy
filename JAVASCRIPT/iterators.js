6880df0e5eaf2bd48e2d565468da86e87763f4e7
Fábio Morais <fabiouds@hotmail.com>
2019-02-25 00:12:46 +0000

    UPDATE

---
+++ JAVASCRIPT/iterators.js
@@ -0,0 +1,51 @@
+/*************************************************************/
+/*****FOR EACH*******/
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+/*Repete o mesmo codigo para cada elemento do vetor*/
+
+fruits.forEach(item => console.log('I want to eat a '+item))
+
+/*OU*/
+
+function print(element){
+  console.log('I want to eat a '+element);
+}
+
+fruits.forEach(print);
+
+/*************************************************************/
+/*MAP*/
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0]})
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => { return number/100})
+
+console.log(smallNumbers)
+
+/*************************************************************/
+/*FILTER*/
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250;
+})
+console.log(smallNumbers)
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter(car => {
+  return car.length > 7
+})
+console.log(longFavoriteWords);
+/*************************************************************/
